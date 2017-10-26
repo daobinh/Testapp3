0b15c3b1a68c141fba388cadbd2f832c35edcd0f
unknown <Dbinh>
2017-10-26 13:51:26 +0700

    fix app.js

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,2 @@
-var app = angular.module('memberRecords', ['ngFileUpload']).constant('API_URL', 'http://localhost/TestApp3/public/');
+// var app = angular.module('memberRecords', ['ngFileUpload']).constant('API_URL', 'http://localhost/TestApp3/public/');
+var app = angular.module('memberRecords', ['ngFileUpload']).constant('API_URL', 'http://150.95.104.135/TestApp3/public/');
